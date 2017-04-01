2e0b32b250f2cf5b37c0e3809c79a1981b0b3dd6
Luis <luis.rivera157@upr.edu>
2017-04-01 10:10:59 -0400

    Answer p3b

--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,5 @@
 #Code here
+import csv
+schoolsFile = sc.textFile("hdfs://..../escuelasPR.csv")
+schoolsData = studentsFile.map(lambda line: line.split(','))
+data = studentsData.filter(lambda x: x[2]=='Ponce' or x[2] == 'Cabo Rojo' or x[2] == 'Dorado')
