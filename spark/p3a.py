5234dbcd028063773a6e31dee3dd3ac45835d0dd
Luis <luis.rivera157@upr.edu>
2017-04-01 09:28:44 -0400

    added spark p3a

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,6 @@
 #Code here
+
+import csv
+studentsFile = sc.textFile("hdfs://..../studentsPR.csv")
+studentsData = studentsFile.map(lambda line: line.split(','))
+data = studentsData.filter(lambda x: x[5]=='F').filter(lambda x: x[2]=='71381')
